e8801438c4b0285ced06d48bedeffa7f8b994344
金正朋也 <mhtnim1023@gmail.com>
2018-06-07 13:40:03 +0900

    二分探索を実際にやってみる

---
+++ binary_search.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    list = [1, 3, 5, 9, 18, 20]
+    low = 0
+    high = len(list) - 1
+    item = 1 #検索したい数値
+    
+    while low <= high:
+        mid = (low + high) // 2 #//で、小数点以下切り捨てることができる
+        guess = list[mid]
+        if guess == item:
+            print(f"見つかりました！{item}")
+            break
+        elif guess < item:
+            low = mid + 1
+        elif guess > item:
+            high = mid - 1
+    if guess != item:
+        print(f"{item}は、listに存在しません")
