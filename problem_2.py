63148b68b6fd9bfde3f12053462bfc33d669f39c
金正朋也 <mhtnim1023@gmail.com>
2018-06-16 01:14:49 +0900

    フィボナッチ数列修正

--- problem_2.py
+++ problem_2.py
@@ -1,4 +1,13 @@
-# フィボナッチ数列
+# １. フィボナッチ数列（ジェネレータ）
+def fib(n):
+    a, b = 1, 2
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+
+print([i for i in fib(10)])
+
+# 2. appendでフィボナッチ数列を作成
 fibo = [1, 2]
 sum_array = []
 while sum(fibo) < 4000000:
@@ -9,3 +18,4 @@ for i in range(len(fibo)):
     if i % 2 == 0:
         sum_array.append(fibo[i])
 print(sum(sum_array))
+
